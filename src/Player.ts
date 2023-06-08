a9bc82242311ab21679be667c577632a80a4640e
Ian Ong <ongmichaelian@gmail.com>
2023-06-08 09:48:49 -0400

    Update Player.ts

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,6 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(10);
+    betCallback(100);
   }
 
   public showdown(gameState: any): void {
