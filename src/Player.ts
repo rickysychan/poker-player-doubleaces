856c7123c41679170113bf21bb6ef31c51a83654
Ian Ong <ongmichaelian@gmail.com>
2023-06-08 14:28:31 -0400

    Update Player.ts

--- src/Player.ts
+++ src/Player.ts
@@ -40,15 +40,50 @@ export class Player {
         return;
       }
     } else if (community_cards.length >= 3) {
-      var bet = this.checkAllCards(hole_cards, community_cards);
+      var bet = this.checkAllCards(gameState, hole_cards, community_cards);
       betCallback(bet);
       return;
     }
 
     betCallback(250);
   }
+  
+  public isFlush(hole_cards: Card[], community_cards: Card[]): boolean {
+    var combination = hole_cards.concat(community_cards);
+    if (combination.length < 5) {
+      return false;
+    }
 
-  public checkAllCards(hole_cards: Card[], community_cards: Card[]): number {
+    this.sortBySuit(combination);
+
+    return combination[0].suit == combination[4].suit;
+  }
+
+  public sortBySuit(cards: Card[]) {
+    var i: number, j: number, min_j: number;
+
+    for (i = 0 ; i < cards.length ; i++) {
+      min_j = i;   // Assume elem i (h[i]) is the minimum
+
+      for (j = i+1 ; j < cards.length; j++) {
+        if ( cards[j].suit < cards[min_j].suit ) {
+          min_j = j;    // We found a smaller suit value, update min_j     
+        }
+      }
+
+      var help = cards[i];
+      cards[i] = cards[min_j];
+      cards[min_j] = help;
+    }
+
+    return cards;
+  }
+
+  public checkAllCards(gameState: any, hole_cards: Card[], community_cards: Card[]): number {
+    if (this.isFlush(hole_cards, community_cards)) {
+      return this.getPlayer(gameState).stack;
+    }
+    
     return 250; //TODO: Change this!!!
   }
 
