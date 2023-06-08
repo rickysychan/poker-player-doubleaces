39a8e4f5156eff3a06f02270a5c9d39fbaf66b08
Greg Yaniw <34722954+GregYaniw@users.noreply.github.com>
2023-06-08 10:34:30 -0400

    Update Player.ts

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,12 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
     console.log('Game State: ', gameState);
+    
+    try {
+      console.log('Hole Cards', gameState.players.find((e) => e.name === 'DoubleAces').hole_cards);
+    } catch(error) {
+      console.log('Hole Cards', error);
+    }
     betCallback(1000);
   }
 
